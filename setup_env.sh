70ed63b868e697d5ba293bed97f9d9de3657ae60
Jose Williams Garcia <jwgarcia003@gmail.com>
2023-08-09 19:02:40 -0400

    setup enviroment variables

--- setup_env.sh
+++ setup_env.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
-echo DEBUG=$DEBUG >> .env
-echo SQL_ENGINE=django.db.backends.postgresql >> .env
-echo DATABASE=postgres >> .env
-
-#echo SECRET_KEY=$SECRET_KEY >> .env
-echo ID_COMPILACION=$CI_COMMIT_SHORT_SHA >> .env
-echo SERVER_IP=$SERVER_IP >> .env
-echo SITE_URL=$SITE_URL >> .env
-echo POSTGRES_DB=$SQL_DATABASE >> .env
-echo POSTGRES_USER=$SQL_USER >> .env
-echo POSTGRES_PASSWORD=$SQL_PASSWORD >> .env
-echo SQL_HOST=$SQL_HOST >> .env
-echo SQL_PORT=$SQL_PORT >> .env
-echo WEB_IMAGE=$IMAGE:web  >> .env
-echo NGINX_IMAGE=$IMAGE:nginx  >> .env
-echo TRAEFIK_IMAGE=$IMAGE:traefik  >> .env
-echo CI_REGISTRY_USER=$CI_REGISTRY_USER   >> .env
-echo CI_JOB_TOKEN=$CI_JOB_TOKEN  >> .env
-echo CI_REGISTRY=$CI_REGISTRY  >> .env
-echo IMAGE=$CI_REGISTRY/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME >> .env
-echo SENDGRID_API_KEY=$SENDGRID_API_KEY  >> .env
-echo CELERY_BROKER_URL=$CELERY_BROKER_URL >> .env
+echo "DEBUG=${{ secrets.DEBUG }}" >> .env
+echo "SQL_ENGINE=django.db.backends.postgresql" >> .env 
+echo "DATABASE=postgres" >> .env
+echo "ID_COMPILACION=$GITHUB_SHA" >> .env  
+echo "SERVER_IP=${{ secrets.SERVER_IP }}" >> .env
+echo "SITE_URL=${{ secrets.SITE_URL }}" >> .env
+echo "POSTGRES_DB=${{ secrets.SQL_DATABASE }}" >> .env
+echo "POSTGRES_USER=${{ secrets.SQL_USER }}" >> .env
+echo "POSTGRES_PASSWORD=${{ secrets.SQL_PASSWORD }}" >> .env
+echo "SQL_HOST=${{ secrets.SQL_HOST }}" >> .env
+echo "SQL_PORT=${{ secrets.SQL_PORT }}" >> .env
+echo "WEB_IMAGE=${{ env.IMAGE }}:web" >> .env
+echo "NGINX_IMAGE=${{ env.IMAGE }}:nginx" >> .env 
+echo "TRAEFIK_IMAGE=${{ env.IMAGE }}:traefik" >> .env
+echo "CI_REGISTRY_USER=${{ secrets.REGISTRY_USER }}" >> .env
+echo "CI_JOB_TOKEN=${{ secrets.REGISTRY_PASSWORD }}" >> .env
+echo "CI_REGISTRY=${{ secrets.REGISTRY_URL }}" >> .env
+echo "IMAGE=${{ secrets.REGISTRY_URL }}/$GITHUB_REPOSITORY" >> .env  
+echo "SENDGRID_API_KEY=${{ secrets.SENDGRID_KEY }}" >> .env
+echo "CELERY_BROKER_URL=${{ secrets.CELERY_BROKER }}" >> .env
