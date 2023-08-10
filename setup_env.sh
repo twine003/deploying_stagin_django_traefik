67caac0910d3d2c66de6e813241ddb4ee9b314eb
Jose Williams Garcia <jwgarcia003@gmail.com>
2023-08-09 20:02:28 -0400

    setup enviroment variables

--- setup_env.sh
+++ setup_env.sh
@@ -11,12 +11,13 @@ echo "POSTGRES_USER=$SQL_USER" >> .env
 echo "POSTGRES_PASSWORD=$SQL_PASSWORD" >> .env
 echo "SQL_HOST=db" >> .env
 echo "SQL_PORT=$SQL_PORT" >> .env
+
+echo "IMAGE=$IMAGE" >> .env  
 echo "WEB_IMAGE=$IMAGE:web" >> .env
 echo "NGINX_IMAGE=$IMAGE:nginx" >> .env 
 echo "TRAEFIK_IMAGE=$IMAGE:traefik" >> .env
 
 echo "ID_COMPILACION=$GITHUB_SHA" >> .env  
-echo "CI_REGISTRY_USER=$REGISTRY_USER" >> .env
-echo "CI_JOB_TOKEN=$REGISTRY_PASSWORD" >> .env
-echo "CI_REGISTRY=$REGISTRY_URL" >> .env
-echo "IMAGE=$REGISTRY_URL/$GITHUB_REPOSITORY" >> .env  
+# echo "CI_REGISTRY_USER=$REGISTRY_USER" >> .env
+# echo "CI_JOB_TOKEN=$REGISTRY_PASSWORD" >> .env
+# echo "CI_REGISTRY=$REGISTRY_URL" >> .env
