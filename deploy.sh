15c6569ce14de539a6279a58df6862798b523090
Jose Williams Garcia <jwgarcia003@gmail.com>
2023-08-13 14:16:58 -0400

    Deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << 'ENDSSH'
+  cd /root
+  export $(cat .env | xargs)
+  docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+  docker pull $WEB_IMAGE
+  docker pull $NGINX_IMAGE
+  pwd
+  ls -lsa
+  docker-compose -f docker-compose.prod.traefik.yml up -d --force-recreate --build
+  docker image prune -a --filter "until=15m"
+ENDSSH
