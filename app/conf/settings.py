75de11151f1d83aa1f7ed042e02fdc31ffa44c08
Jose Williams Garcia <jwgarcia003@gmail.com>
2023-08-15 18:37:54 -0400

    deploy

--- app/conf/settings.py
+++ app/conf/settings.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "yolo",
+    "django.contrib.staticfiles"
 ]
 
 MIDDLEWARE = [
